06a508ff79ebebadea57f4c3a89dda68e119754f
Matt Bradshaw <mattbrad@uk.ibm.com>
2018-04-24 10:05:12 +0100

    brew model attribute changes

--- models/brew.js
+++ models/brew.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
 const dbConn = mongoose.createConnection('mongodb://localhost:27017/nanobrew');
+const moment = require('moment');
 
 // define the schema for our user model
 var brewSchema = mongoose.Schema({
-    name            : {type: String, required: true},
+    name                : {type: String, required: true},
     minTemp             : {type: Number, required: true},
     maxTemp             : {type: Number, required: true},
     topic               : {type: String, required: true},
-    created             : Number,
+    created             : {type: Number, default:new moment()},
     finished            : Number,
-    inProgress          : Boolean,
+    stopped             : false,
     readings            : {
         id  :   {
             type: mongoose.Schema.Types.ObjectId,
