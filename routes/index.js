09a87276ea7aee8ccb8992dc999e0e64cba6aeed
Matt Bradshaw <mattbrad@uk.ibm.com>
2018-04-24 14:30:38 +0100

    bootstrap 4 on the homepage and changing it to be ejs rather than static html

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (req, res)=>{
-    res.redirect('/index.html');
+    res.render('index');
 });
 
 router.get("/index", (req, res)=>{
-    res.redirect('/index.html');
+    res.redirect('/');
 });
 
 router.get("*", (req, res)=>{
