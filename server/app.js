7df559a5171cee83c6fb3afdd2163fb1ae7211e9
Matt Bradshaw <mattbrad@uk.ibm.com>
2018-04-24 10:05:41 +0100

    applying method overide for delete put requests

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const methodOverride = require("method-override");
 
 //Project dependencies
 const indexRoutes = require('../routes/index');
@@ -10,11 +11,13 @@ const readingRoutes = require('../routes/reading');
 
 //Setting up the express server
 const app = express();
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(express.static('public'));
-const port = process.env.port || 3000;
+app.use(methodOverride("_method"));
 
+const port = process.env.port || 3000;
 
 //Import routes for brews, readings and other
 app.use(brewRoutes);
