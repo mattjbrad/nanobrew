971cd860b227dccc143852874c3a326893ff8f2b
Matt Bradshaw <mattbrad@uk.ibm.com>
2018-04-27 14:48:36 +0100

    Fixing reading schema

--- models/reading.js
+++ models/reading.js
@@ -1,16 +1,10 @@
 var mongoose = require('mongoose');
-var brewDataConn = mongoose.createConnection('mongodb://localhost:27017/nanobrew');
 
 // define the schema for our user model
 var readingSchema = mongoose.Schema({
-
-    reading            : {
-        topic       : {type: String, required:true},
-        date        : {type: Date, required:true},
+        time        : {type: Number, default:new Date().getTime()},
         temp        : {type: Number, required:true}
-    }
-
 });
 
 // create the model for readings and expose it to our app
-module.exports = brewDataConn.model('Reading', readingSchema);
+module.exports = mongoose.model('Reading', readingSchema);
