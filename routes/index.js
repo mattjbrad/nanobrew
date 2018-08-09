78c15438d3e16f83b67f6b1200f3f608b29894e4
Matt Bradshaw <mattbrad@uk.ibm.com>
2018-08-09 15:56:57 +0100

    basic login page and minor formatting changes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/", (req, res)=>{
+router.get("/", (req, res) => {
     res.render('index');
 });
 
-router.get("/index", (req, res)=>{
+router.get("/index", (req, res) => {
     res.redirect('/');
+});
+
+router.get("/register", (req, res) =>{ 
+    res.render('register');
+});
+
+router.get("/login", (req, res) => {
+    res.render('login');
+});
+
+router.post("/login", (req, res) => {
+    res.send(`Logged in as ${req.body.email}`)
 });
 
 router.get("*", (req, res)=>{
